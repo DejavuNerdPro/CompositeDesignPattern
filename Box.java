2271289810ef1df207bd8a316dce4b1ed2459ad4
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2023-12-31 01:35:46 +0630

    box implenmentation

--- Box.java
+++ Box.java
@@ -1,3 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Box {
+    private List<Box> boxList = new ArrayList<>();
+    private List<Object> objectList = new ArrayList<>();
+
+    public Box() {
+    }
+
+    public Box(List<Box> boxs, List<Object> objects) {
+        this.boxList = boxs;
+        this.objectList = objects;
+    }
+
+    public double calculatePrice(){
+        double sum=objectList.stream().mapToDouble(object::getPrice()).sum();
+        for(Box box:boxList){
+            sum=+box.calculatePrice();
+        }
+        return sum;
+    }
+
+    public List<Box> getBoxList() {
+        return boxList;
+    }
+
+    public void setBoxList(List<Box> boxList) {
+        this.boxList = boxList;
+    }
+
+    public List<Object> getObjectList() {
+        return objectList;
+    }
+
+    public void setObjectList(List<Object> objectList) {
+        this.objectList = objectList;
+    }
 
 }
